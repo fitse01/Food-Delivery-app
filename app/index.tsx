f92a5cb224e17241fac8ad9704d24ebf35e4ac36
Fitsum Tafese <fitsumtafese01@gmail.com>
2025-07-30 00:28:15 +0300

    home page UI Complete

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,3 @@
-import CartButton from "@/components/CartButton";
-import { images, offers } from "@/constants";
 import cn from "clsx";
 import { Fragment } from "react";
 import {
@@ -11,13 +9,20 @@ import {
   View,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+
+import CartButton from "@/components/CartButton";
+import { images, offers } from "@/constants";
+
 export default function Index() {
+  // const { user } = useAuthStore();
+
   return (
     <SafeAreaView className="flex-1 bg-white">
       <FlatList
         data={offers}
         renderItem={({ item, index }) => {
           const isEven = index % 2 === 0;
+
           return (
             <View>
               <Pressable
@@ -30,13 +35,14 @@ export default function Index() {
               >
                 {({ pressed }) => (
                   <Fragment>
-                    <View className="h-full w-1/2">
+                    <View className={"h-full w-1/2"}>
                       <Image
                         source={item.image}
                         className={"size-full"}
                         resizeMode={"contain"}
                       />
                     </View>
+
                     <View
                       className={cn(
                         "offer-card__info",
@@ -61,10 +67,10 @@ export default function Index() {
         }}
         contentContainerClassName="pb-28 px-5"
         ListHeaderComponent={() => (
-          <View className=" flex-between flex-row w-full my-5 ">
-            <View className="flex-start ">
-              <Text className="small-bold  text-primary">DELIVER TO</Text>
-              <TouchableOpacity className="flex-center flex-row gap-x-1  mt-0.5">
+          <View className="flex-between flex-row w-full my-5">
+            <View className="flex-start">
+              <Text className="small-bold text-primary">DELIVER TO</Text>
+              <TouchableOpacity className="flex-center flex-row gap-x-1 mt-0.5">
                 <Text className="paragraph-bold text-dark-100">Adama</Text>
                 <Image
                   source={images.arrowDown}
@@ -73,6 +79,7 @@ export default function Index() {
                 />
               </TouchableOpacity>
             </View>
+
             <CartButton />
           </View>
         )}
--- components/CartButton.tsx
+++ components/CartButton.tsx
@@ -1,11 +1,19 @@
+import { images } from "@/constants";
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 
 const CartButton = () => {
+  const totalItems = 10;
   return (
-    <View>
-      <Text>CartButton</Text>
-    </View>
+    <TouchableOpacity className="cart-btn" onPress={() => {}}>
+      <Image source={images.bag} className="size-5" resizeMode="contain" />
+
+      {totalItems > 0 && (
+        <View className="cart-badge">
+          <Text className="small-bold text-white">{totalItems}</Text>
+        </View>
+      )}
+    </TouchableOpacity>
   );
 };
 
