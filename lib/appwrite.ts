c315d5dadd3e998989862362e10988754a3ae0cf
fitse01 <fitsumtafese01@gmail.com>
2025-08-01 11:40:43 +0300

    SetUp  AppWrite

---
+++ lib/appwrite.ts
@@ -0,0 +1,5 @@
+export const appwriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT || "https://nyc.cloud.appwrite.io/v1",
+    project: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID || "688c77fc00079e445344",
+    platform: "com.fitsum.foodordering "
+}
