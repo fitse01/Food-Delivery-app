871cd4fa0a684ef90db16832c8f6bd003b75e7c6
fitse01 <fitsumtafese01@gmail.com>
2025-08-09 16:50:02 +0300

    Tab Navigation Setup

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -12,10 +12,10 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 import CartButton from "@/components/CartButton";
 import { images, offers } from "@/constants";
-import useAuthStore from "@/store/auth.store";
+// import useAuthStore from "@/store/auth.store";
 
 export default function Index() {
-  const { user } = useAuthStore();
+  // const { user } = useAuthStore();
   // console.log("useAuthStore user:", JSON.stringify(user, null, 2));
 
   return (
