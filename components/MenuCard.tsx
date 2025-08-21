96faa012bebe1f50a6162f83a09b7336312ad85e
fitse01 <fitsumtafese01@gmail.com>
2025-08-21 07:02:42 +0300

    cart page Completed

--- components/MenuCard.tsx
+++ components/MenuCard.tsx
@@ -1,9 +1,15 @@
 import { appwriteConfig } from "@/lib/appwrite";
+import { useCartStore } from "@/store/cart.store";
 import { MenuItem } from "@/type";
 import { Image, Platform, Text, TouchableOpacity } from "react-native";
 
-const MenuCard = ({ item: { image_url, name, price } }: { item: MenuItem }) => {
+const MenuCard = ({
+  item: { $id, image_url, name, price },
+}: {
+  item: MenuItem;
+}) => {
   const imageUrl = `${image_url}?project=${appwriteConfig.projectId}`;
+  const { addItem } = useCartStore();
   return (
     <TouchableOpacity
       className="menu-card"
@@ -26,7 +32,17 @@ const MenuCard = ({ item: { image_url, name, price } }: { item: MenuItem }) => {
         {name}
       </Text>
       <Text className="body-regular text-gray-200 mb-4">From ${price}</Text>
-      <TouchableOpacity onPress={() => {}}>
+      <TouchableOpacity
+        onPress={() =>
+          addItem({
+            id: $id,
+            image_url: imageUrl,
+            name,
+            price,
+            customizations: [],
+          })
+        }
+      >
         <Text className="paragraph-bold text-primary">Add to Cart +</Text>
       </TouchableOpacity>
     </TouchableOpacity>
